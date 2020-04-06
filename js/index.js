2ca5a425cd00003ea898abfdedad58d79fd1b26e
Cornelia1991 <Cornelia1991@users.noreply.github.com>
2020-04-05 21:14:43 -0400

    added animation to stroll

---
+++ js/index.js
@@ -0,0 +1,12 @@
+//modal for project cards
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+  $('.modal').modal();
+  });
+
+
+  //for stroll animation
+  AOS.init({
+          
+    duration: 1000,
+  });
